7a2b7b9cd4d7537276bf8387a11c0fbf62a29f42
khoefli <khoeflinger@t-online.de>
2026-05-31 13:51:41 +0200

    Add files via upload

---
+++ src/Config.mk
@@ -0,0 +1,5 @@
+# Config file for Agmonis/src/main/tex
+# 2025-01-07 created by Klaus Hoeflinger
+
+LUATEX = C:\texlive\2023\bin\windows\lualatex -output-directory ../target
+LUATEX = lualatex                             -output-directory ../target
